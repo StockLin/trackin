2297b906e5a65fd355a99b018601ee19eaa7aa28
stockLin <a6898208@gmail.com>
2018-02-25 01:37:08 -0800

    create user status identify dealing and detail templates identify whether user has auth to see pages.

--- Strategy/views.py
+++ Strategy/views.py
@@ -34,10 +34,27 @@ def listing(request, category='bull', level='normal'):
 
 @login_required(login_url='/users/login/')
 def details(request, s_id):
+    # strategies level name to user status dictionary
+    LEVEL_DIC = {
+        'normal':0,
+        'golden':1,
+        'platinum':2,
+        'diamond':3,
+    }
+
+# the constant which can let user see strategy detail
+    PASS = None
+
+# save error messages
+    ERROR_MSG_HEAD = None
+    ERROR_MSG = None
+
     # identify the session whether has already logined
     if request.user.is_authenticated():
         username = request.user.username
 
+
+# trying to get use profile
     try:
         user = User.objects.get(username = username)
         # get user profile
@@ -46,13 +63,31 @@ def details(request, s_id):
     except:
         user_status = 0
 
+
+# trying to get specific strategy by it's id
     try:
+        # getting specific strategy by id
         results = strategy.objects.get(id=s_id)
-        # identify whether getting the data by id
-        if results:
-            return render(request, 'strategies/details.html', locals())
-        else:
-            return render(request, 'details.html', locals())
-            # messages.add_message(request, messages.WARNING, '策略不存在.')
+        # transformimg strategies level string to user status numbers by using LEVEL_DIC dictionary
+        s_level = LEVEL_DIC[results.level]
+
     except:
-        return redirect('/strategies')
+        results = None
+        # return redirect('/strategies')
+
+
+    # identify whether data is gotten by it's id
+    if results:
+        # identify whether user auth is same as strategies level
+        if user_status >= s_level:
+            PASS = 1
+        else:
+            ERROR_MSG_HEAD = '會員等級不足'
+            ERROR_MSG = '你的會員等級不足，請升級到指定的會員等級.'
+    else:
+        ERROR_MSG_HEAD = '頁面連結錯誤'
+        ERROR_MSG = '很抱歉！您所要進入的頁面連結可能有誤，您可到其他網頁瀏覽或是使用搜尋功能查詢其他商品資訊。'
+        # messages.add_message(request, messages.WARNING, '策略不存在.')
+
+    return render(request, 'strategies/details.html', locals())
+
